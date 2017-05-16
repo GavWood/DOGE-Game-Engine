327d88b399122f989ff02c13e4a421f01b13ccb3
Gavin Wood <gavinneilwood@gmail.com>
2017-05-16 17:34:13 +0100

    restored SbMain.h and cpp

---
+++ GameFramework_Tutorials/3DScene/3DScene/SourceCode/SbMain.h
@@ -0,0 +1,109 @@
+////////////////////////////////////////////////////////////////////////////////
+// SbMain.h
+
+// Include guard
+#pragma once
+#include "BtTypes.h"
+#include "BaArchive.h"
+#include "MtVector3.h"
+#include "RsVertex.h"
+#include "SbCamera.h"
+#include "SdSound.h"
+#include "SbRenderSkybox.h"
+#include "SbModel.h"
+#include "GaProject.h"
+#include "SdSound.h"
+#include "Game.h"
+#include "HlJoysticks.h"
+#include "SbBubbles.h"
+#include "SbFrustum.h"
+
+class SbMain;
+class RsTexture;
+class RsShader;
+class RsMaterial;
+class SgNode;
+
+// Class definition
+class SbMain : public GaProject
+{
+public:
+
+	// Public functions
+	void							Init();
+	void							Reset();
+	void							Create();
+	void							Update();
+	void							Render();
+	void							PreRender();
+	void							PostRender();
+	void							Destroy();
+ 
+    void                            RenderIntoHMD( RsMaterial *pMaterial,
+                                                   const MtVector2 &v2Position,
+                                                   MtVector2 &v2Dimension,
+                                                   const MtVector2 &v2ScreenDimension,
+                                                   BtU32 sortOrder
+                                                 );
+    
+	// Accessors
+	void							SetClosing();
+	BtBool							IsClosed();
+	BtBool							IsClosing();
+
+private:
+
+	// Private functions
+	void							RestoreRenderTarget();
+	void							Render2DScene();
+	void							Render3DScene();
+	void							Render2DInto3D( RsCamera &camera );
+
+	void							SetupRenderToTexture( RsTexture *pTexture, RsCamera camera );
+	void							SetupRender();
+	void							RenderFinal();
+	void							PlaySound();
+	void							UpdateTest();
+	void							RenderTests();
+	void							RenderFont();
+	void							DrawRenderTarget();
+	
+	// Private members
+	BaArchive						m_gameArchive;
+	BaArchive						m_animArchive;
+	BaArchive						m_utilityArchive;
+	BaArchive						m_hmdArchive;
+	
+	// Resources
+	HlJoysticks						m_joysticks;
+	RsMaterial					   *m_pLeftEye;
+	RsMaterial					   *m_pRightEye;
+
+	RsShader					   *m_pShader;
+	SbCamera						m_camera;
+	RsMaterial					   *m_pWhite2;
+	RsMaterial					   *m_pWhite3;
+	RsMaterial					   *m_pGUIRenderTarget;
+
+	// Test classes
+	SbModel							m_model;
+	SbRenderSkybox					m_skybox;
+	SbBubbles						m_bubbles;
+	SbFrustum						m_frustum;
+	RsVertex3						m_v3Vertex[6];
+};
+
+inline void SbMain::SetClosing()
+{
+	m_isClosing = BtTrue;
+}
+
+inline BtBool SbMain::IsClosed()
+{
+	return m_isClosed;
+}
+
+inline BtBool SbMain::IsClosing()
+{
+	return m_isClosing;
+}
