2eb8d0b35cea3df8a44aae47538e6906b8b75cf6
Gavin Wood <gavinneilwood@gmail.com>
2017-09-03 10:44:11 +0100

    ...

---
+++ GameFramework/StruffyLiteWinGL/RenderSystem/RsPlatform.h
@@ -0,0 +1,14 @@
+//
+//  RsPlatform.h
+//  Packer_OSX
+//
+//  Created by localadmin on 23/07/2017.
+//  Copyright © 2017 localadmin. All rights reserved.
+//
+
+#pragma once
+#ifdef WIN32
+#include <GL/glew.h>
+#else
+#include "glew.h"
+#endif
